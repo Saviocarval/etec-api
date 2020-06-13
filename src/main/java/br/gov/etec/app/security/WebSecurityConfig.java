29b6b9f838efe27831ee42e2c5a44a9d644519ad
Savio Carvalho <savio_carvalho@outlook.com>
2020-06-12 22:51:57 -0300

    produção

--- src/main/java/br/gov/etec/app/security/WebSecurityConfig.java
+++ src/main/java/br/gov/etec/app/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and().authorizeRequests()
-			.antMatchers("/auth/**")
+			.antMatchers("/auth/**","/swagger-ui.html")
 			.permitAll().anyRequest().authenticated();
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(),
 					UsernamePasswordAuthenticationFilter.class);
